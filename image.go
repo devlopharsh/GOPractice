0787fbb9bf07e3c97eb73db7f04cbea0410834c8
developharsh <harsh237hk@gmail.com>
2025-06-01 15:12:59 +0530

    practice day 2

---
+++ image.go
@@ -0,0 +1,19 @@
+package main
+
+
+
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		row := make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			row[j]=uint8(( i ^ j))
+		}
+		result[i]=row
+	}
+	return result
+}
+
+// func main() {
+	
+// }
--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main 
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-type Sample struct{
-	x int
-	y string
-}
+import "golang.org/x/tour/pic"
+
+// type Sample struct{
+// 	x int
+// 	y string
+// }
 
 func main(){
-	// sample :=Sample{1, "Hello"}	
-	// fmt.Println(sample.x, sample.y)
-	//arrays in go 
-	var ar [5] int
-	for i:=0 ; i<5 ; i++{
-		ar[i]=i
-	} 
-	fmt.Println(ar)
+	// // sample :=Sample{1, "Hello"}	
+	// // fmt.Println(sample.x, sample.y)
+	// //arrays in go 
+	// var ar [5] int
+	// for i:=0 ; i<5 ; i++{
+	// 	ar[i]=i
+	// } 
+	// fmt.Println(ar)
+	pic.Show(Pic)
 
 }
