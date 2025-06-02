b677229ddd6a902071d8a9de1974a070e21fcf18
developharsh <harsh237hk@gmail.com>
2025-06-02 21:50:38 +0530

    daily practice

--- main.go
+++ main.go
@@ -1,36 +1,57 @@
+// package main
+
+// import "fmt"
+
+// // import (
+// // 	"fmt"
+// // )
+
+// // import "golang.org/x/tour/pic"
+
+// // type Sample struct{
+// // 	x int
+// // 	y string
+// // }
+// type Vertex struct{
+// 	Lat , Long float64
+// }
+
+// var m map[string]Vertex
+
+// func main(){
+// 	// // sample :=Sample{1, "Hello"}
+// 	// // fmt.Println(sample.x, sample.y)
+// 	// //arrays in go
+// 	// var ar [5] int
+// 	// for i:=0 ; i<5 ; i++{
+// 	// 	ar[i]=i
+// 	// }
+// 	// fmt.Println(ar)
+// 	// pic.Show(Pic)
+// 	m = make(map[string]Vertex)
+// 	m["Bells"]=Vertex{
+// 		40 ,50 ,
+// 	}
+// 	fmt.Println(m["Bells"])
+// }
+
 package main
 
-import "fmt"
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
 
-// import (
-// 	"fmt"
-// )
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	count := make(map[string]int)
 
-// import "golang.org/x/tour/pic"
-
-// type Sample struct{
-// 	x int
-// 	y string
-// }
-type Vertex struct{
-	Lat , Long float64
+	for _,word := range words{
+		count[word]++
+	}
+	return count
 }
 
-var m map[string]Vertex
-
-func main(){
-	// // sample :=Sample{1, "Hello"}	
-	// // fmt.Println(sample.x, sample.y)
-	// //arrays in go 
-	// var ar [5] int
-	// for i:=0 ; i<5 ; i++{
-	// 	ar[i]=i
-	// } 
-	// fmt.Println(ar)
-	// pic.Show(Pic)
-	m = make(map[string]Vertex)
-	m["Bells"]=Vertex{
-		40 ,50 ,
-	}
-	fmt.Println(m["Bells"])
+func main() {
+	wc.Test(WordCount)
 }
